8a7d4ebef10a5daaba7a7680029b62600741f9ff
fahadusmani20 <fahad.usmani2020@vitbhopal.ac.in>
2024-04-23 16:51:58 +0530

    Updated code for Reversing Array

--- Arrays/reverseArray.cpp
+++ Arrays/reverseArray.cpp
@@ -5,29 +5,49 @@ using namespace std;
 
 void reverseArray(int arr[], int size)
 {
-    int i = 0, j = size - 1;
+    int start = 0, end = size - 1;
 
-    while(i < j)
+    while(start < end)
     {
-        swap(arr[i], arr[j]);
-        i++; j--;
+        swap(arr[start], arr[end]);
+        start++; end--;
     }
+}
+
+void take_input(int arr[], int size)
+{
+    cout << "Enter Element: ";
+    for(int i = 0; i < size ; i++)
+    {
+        cin >> arr[i];  
+    }
+}
+
+void print_Array(int arr[], int size)
+{
+    for(int i =0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
     int arr[100];
+
+    int size;
+
+    cout << "Enter size of array: ";
+    cin >> size;
     
-    for(int i = 0; i < 6 ; i++)
-    {
-        cout << "Enter Element: ";
-        cin >> arr[i];  
-    }
+    take_input(arr, size);
+
+    cout << "Given Array: ";
+    print_Array(arr, size);
 
     reverseArray(arr, 6);
 
-    for(int i =0; i < 6; i++)
-    {
-        cout << arr[i] << endl;
-    }
+    cout << "Updated Array: ";
+    print_Array(arr, size);
 }
