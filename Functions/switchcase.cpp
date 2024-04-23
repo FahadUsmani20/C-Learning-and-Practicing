780cb95b4882b1fbc4cce2a90c8423826fa55838
fahadusmani20 <fahad.usmani2020@vitbhopal.ac.in>
2024-04-23 09:17:02 +0530

    Added Switch Case

---
+++ Functions/switchcase.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    switch(num)
+    {
+        case 1: 
+        cout << "First" << endl;
+        break;
+
+        case 2:
+        cout << "Second" <<endl;
+        break;
+
+        default:
+        cout << "It is default case" << endl;
+    }
+
+    return 0;
+}
