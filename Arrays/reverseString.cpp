b179eb1c0fa0a069b1bcb962dbc344c9eff767fc
fahadusmani20 <fahad.usmani2020@vitbhopal.ac.in>
2024-04-25 11:41:09 +0530

    Solved Reverse String

---
+++ Arrays/reverseString.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+
+using namespace std;
+
+int get_length(char arr[])
+{
+    int count = 0;
+    
+    while(arr[count] != '\0')
+    {
+        count++;
+    }
+
+    return count;
+}
+
+void reverseString(char arr[], int length)
+{
+    int start = 0, end = length-1;
+
+    while(start < end)
+    {
+        swap(arr[start], arr[end]);
+        start++, end--;
+    }
+}
+
+int main()
+{
+    char names[10];
+
+    cout << "Enter Name: ";
+    cin >> names;
+    int length = get_length(names);
+    reverseString(names, length);
+    cout << "Reverse: " << names;
+    
+
+    return 0;
+}
